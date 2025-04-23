6b394b993473188a78ae53e6f9fc89a8ed0d8430
zero <zero.kwok@foxmail.com>
2025-04-23 14:53:27 +0800

    Update to 2025-04-23
    - Update MSVC build patches for idevicerestore, libimobiledevice, libirecovery, libplist, and libusbmuxd
    - Adjust libirecovery to a dynamic library

--- patchs/libirecovery/msvc.h
+++ patchs/libirecovery/msvc.h
@@ -14,4 +14,17 @@
 #   define strcasecmp _stricmp
 #endif
 
+#include <windows.h>
+
+// https://stackoverflow.com/questions/5801813/c-usleep-is-obsolete-workarounds-for-windows-mingw
+void usleep(__int64 usec) { 
+    HANDLE timer; 
+    LARGE_INTEGER ft; 
+    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+    timer = CreateWaitableTimer(NULL, TRUE, NULL); 
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
+    WaitForSingleObject(timer, INFINITE); 
+    CloseHandle(timer); 
+}
+
 #endif // msvc_h__
