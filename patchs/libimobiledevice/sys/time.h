57721db40c7158f6914daa7acc9240c31d26bda0
zero <zero.kwok@foxmail.com>
2025-09-29 15:13:44 +0800

    feat(libimobiledevice): 添加 afcclient 工具支持
    在 CMakeLists.txt 中新增 afcclient 工具的编译与安装配置，
    并在 sys/time.h 中增加 Windows 平台下的 gettimeofday 实现，
    以支持在 Windows 环境中使用相关功能。

---
+++ patchs/libimobiledevice/sys/time.h
@@ -0,0 +1,26 @@
+#include <Windows.h>
+
+inline int gettimeofday(struct timeval *tv, struct timezone *tz)
+{
+	// 定义FILETIME结构体
+	FILETIME ft;
+	// 获取当前系统时间
+	GetSystemTimeAsFileTime(&ft);
+
+	// 将FILETIME结构体中的时间值转换为64位整数，单位为100纳秒
+	ULARGE_INTEGER uliTime;
+	uliTime.LowPart = ft.dwLowDateTime;
+	uliTime.HighPart = ft.dwHighDateTime;
+	uint64_t ullTime = uliTime.QuadPart;
+
+	// 将时间值转换为自1970年1月1日00:00:00 UTC以来经过的微秒数
+	static const uint64_t EPOCH_DIFFERENCE = 116444736000000000ULL;
+	ullTime -= EPOCH_DIFFERENCE;
+	ullTime /= 10;
+
+	// 将时间值转换为struct timeval结构体
+	tv->tv_sec = ullTime / 1000000;
+	tv->tv_usec = ullTime % 1000000;
+
+	return 0;
+}
